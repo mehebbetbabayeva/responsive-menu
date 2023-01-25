b3b12ab81da4eb412a637b99ded106f162b95f6f
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-01-26 01:38:08 +0400

    finish nav bar

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,12 +1,35 @@
 document.querySelector("#menuBtn").addEventListener("click" ,()=>{
-   let mobileMenu = document.querySelector(".overlay-deactive");
-   mobileMenu.classList.remove("overlay-deactive");
-   mobileMenu.classList.add("overlay-active");
+   let mobileMenu = document.querySelector(".deactive");
+   mobileMenu.classList.remove("deactive");
+   mobileMenu.classList.add("active");
   
 })
 document.querySelector("#backbtn").addEventListener("click" ,()=>{
-    let mobileMenu = document.querySelector(".overlay-active");
-    mobileMenu.classList.remove("overlay-active");
-    mobileMenu.classList.add("overlay-deactive");
+    let mobileMenu = document.querySelector(".overlay");
+    mobileMenu.classList.remove("active");
+    mobileMenu.classList.add("deactive");
    
  })
+
+document.querySelector(".overlay").addEventListener("click" , function(){
+    this.classList.remove("active");
+    this.classList.add("deactive");
+});
+
+
+
+ function navSize(){
+    let width =window.innerWidth;
+    if(width>900){
+            
+            let mobileMenu = document.querySelector(".overlay");
+            mobileMenu.classList.remove("active");
+            mobileMenu.classList.add("deactive");   
+    }
+    else{
+        let mobileMenu = document.querySelector(".overlay");
+        mobileMenu.classList.remove("deactive");
+        mobileMenu.classList.add("active"); 
+    }
+ }
+window.onresize =navSize;
